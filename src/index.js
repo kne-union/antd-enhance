8740dbcb4f89069052de3464997b38b92accdddb
林志鹏 <zhipenglin@knx.com.cn>
2021-02-20 14:04:26 +0800

    添加字体图标组件

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 export {default as preset} from './preset';
 export {default as ConfigProvider} from './components/ConfigProvider';
+export {default as Iconfont} from './components/Iconfont';
 export {default as LoadingButton} from './components/LoadingButton';
 export {default as withLayer} from './hocs/withLayer';
 export {default as withPage} from './hocs/withPage';
