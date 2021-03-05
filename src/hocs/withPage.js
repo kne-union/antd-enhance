251c9519ab600f7e626ef9332a95fb12cfb21f81
林志鹏 <zhipenglin@knx.com.cn>
2021-03-05 14:26:24 +0800

    怎家params支持

--- src/hocs/withPage.js
+++ src/hocs/withPage.js
@@ -1,9 +1,12 @@
 import React, {useState} from 'react';
 import {setting} from '../preset';
 
-export default ((WrappedComponent) => ({data, ...props}) => {
-  const [current, setCurrent] = useState(setting.withPage.initCurrent);
-  return <WrappedComponent {...props} data={Object.assign({}, data, setting.withPage.transform({page: current}))} onPageChange={(page) => {
-    setCurrent(page);
-  }}/>;
+export default ((WrappedComponent) => ({data, params, ...props}) => {
+    const [current, setCurrent] = useState(setting.withPage.initCurrent);
+    return <WrappedComponent {...props}
+                             params={Object.assign({}, params, setting.withPage.transform({page: current}))}
+                             data={Object.assign({}, data, setting.withPage.transform({page: current}))}
+                             onPageChange={(page) => {
+                                 setCurrent(page);
+                             }}/>;
 });
